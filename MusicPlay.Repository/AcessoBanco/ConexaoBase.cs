ae10acfcdac02ac2723b6168b554faba381dbce0
leodavid <leodavid_andre@hotmail.com>
2015-05-07 14:40:33 -0300

    Classe de conexão

---
+++ MusicPlay.Repository/AcessoBanco/ConexaoBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MusicPlay.Repository.AcessoBanco
+{
+    public class ConexaoBase : IDisposable
+    {
+        private static SqlConnection SqlConnection { get; set; }
+        private SqlCommand SqlCommand { get; set; }
+
+        public ConexaoBase()
+        {
+            SqlConnection = new SqlConnection(@"data source=.; Integrated Security=SSPI;Initial Catalog=MusicPlay");
+            SqlConnection.Open();
+        }
+
+        public void AdicionaParameteros(string nomeParametro, object valor)
+        {
+            SqlCommand.Parameters.AddWithValue(nomeParametro, valor);
+        }
+
+        public void ExecutaProcedure(object procedure)
+        {
+            SqlCommand = new SqlCommand(procedure.ToString(), SqlConnection)
+            {
+                CommandType = CommandType.StoredProcedure,
+                CommandTimeout = 99999
+            };
+        }
+
+        public int ExecuteNonQuery()
+        {
+            return SqlCommand.ExecuteNonQuery();
+        }
+
+        public IDataReader ExecuteReader()
+        {
+            return SqlCommand.ExecuteReader();
+        }
+        public void Dispose()
+        {
+            if (SqlConnection.State == ConnectionState.Open)
+            {
+                SqlConnection.Close();
+            }
+        }
+    }
+}
