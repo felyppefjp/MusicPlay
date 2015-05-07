14407d5b8b8fb2073c42b3b819d6f65348168895
edmarjunior <junior.rp_ds2@hotmail.com>
2015-05-07 14:07:14 -0300

    commit01

--- MusicPlay.Web/Controllers/HomeController.cs
+++ MusicPlay.Web/Controllers/HomeController.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace MusicPlay.Web.Controllers
 {
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            // Hello gitHub (teste) Edmar Junior
             return View();
         }
 
