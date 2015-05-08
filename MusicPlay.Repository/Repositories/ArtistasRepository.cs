66b05eb5b6e11c3c82d608b2083b4cd76a036974
leodavid <leodavid_andre@hotmail.com>
2015-05-08 17:24:11 -0300

    Criação das classe MusicasRepository e ArtistasRepository

---
+++ MusicPlay.Repository/Repositories/ArtistasRepository.cs
@@ -0,0 +1,40 @@
+﻿using MusicPlay.Core.Models;
+using MusicPlay.Repository.AcessoBanco;
+
+namespace MusicPlay.Repository.Repositories
+{
+    public class ArtistasRepository
+    {
+        private enum Procedures
+        {
+            Sp_InsArtista,
+            Sp_DelArtista
+        }
+
+        public void PostArtista(Artistas artistas)
+        {
+            using (var cb = new ConexaoBase())
+            {
+                cb.ExecutaProcedure(Procedures.Sp_InsArtista);
+                cb.AdicionaParameteros("@Nom_Artistas", artistas.Nom_Artista);
+                cb.AdicionaParameteros("@Dat_IniCarreira", artistas.Dat_IniCarreira);
+                cb.AdicionaParameteros("@Num_SeqlGenero", artistas.Num_SeqlGenero);
+                cb.AdicionaParameteros("@Cod_UsuaCad", artistas.Cod_UsuaCad);
+                cb.AdicionaParameteros("@Dat_Cad", artistas.Dat_Cad);
+                cb.AdicionaParameteros("@Cod_UsuaAlt", artistas.Cod_UsuaAlt);
+                cb.AdicionaParameteros("@Dat_UltAlt", artistas.Dat_UsuaAlt);
+                cb.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteArtista(int id)
+        {
+            using (var cb = new ConexaoBase())
+            {
+                cb.ExecutaProcedure(Procedures.Sp_DelArtista);
+                cb.AdicionaParameteros("@Num_SeqlArtista", id);
+                cb.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ MusicPlay.Repository/Repositories/MusicasRepository.cs
@@ -0,0 +1,41 @@
+﻿using MusicPlay.Core.Models;
+using MusicPlay.Repository.AcessoBanco;
+
+namespace MusicPlay.Repository.Repositories
+{
+    public class MusicasRepository
+    {
+        private enum Procedures
+        {
+            Sp_InsMusicasa,
+            Sp_DelMusica 
+        }
+
+        public void PostMusica(Musicas musicas)
+        {
+            using (var cb = new ConexaoBase())
+            {
+                cb.ExecutaProcedure(Procedures.Sp_InsMusicasa);
+                cb.AdicionaParameteros("@Nom_Musica", musicas.Nom_Musica);
+                cb.AdicionaParameteros("@Num_SeqlAlbum", musicas.Num_SeqlAlbum);
+                cb.AdicionaParameteros("@Url_Video", musicas.Url_Video);
+                cb.AdicionaParameteros("@Cod_UsuaCad", musicas.Cod_UsuaCad);
+                cb.AdicionaParameteros("@Dat_Cad", musicas.Dat_Cad);
+                cb.AdicionaParameteros("@Cod_UsuaAlt", musicas.Cod_UsuaCad);
+                cb.AdicionaParameteros("@Dat_UltAlt", musicas.Dat_Cad);
+                cb.AdicionaParameteros("@Num_Temo", musicas.Num_Tempo);
+                cb.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteMusica(int i)
+        {
+            using (var cb = new ConexaoBase())
+            {
+                cb.ExecutaProcedure(Procedures.Sp_DelMusica);
+                cb.AdicionaParameteros("@Num_SeqlMusica", i);
+                cb.ExecuteNonQuery();
+            }
+        }
+    }
+}
